a4767f8b8f3377d3370523a20b1652d648d97b26
Javiserna <hipc04.javier@gmail.com>
2022-06-17 15:45:24 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,19 +18,10 @@ def tac():
     t_sec = round(time.time() - _start_time)
     (t_min, t_sec) = divmod(t_sec,60)
     (t_hour,t_min) = divmod(t_min,60)
-    st.write('{} seg'.format(t_sec))
-
-
-
-#last_received = psutil.net_io_counters().bytes_recv
-#last_sent = psutil.net_io_counters().bytes_sent
-#last_total = last_received + last_sent
+    st.write('{}seg'.format(t_sec))
 
 @st.experimental_memo(suppress_st_warning=True, show_spinner=False)
 def runner(starName):
-    #bytes_received = psutil.net_io_counters().bytes_recv
-    #bytes_sent = psutil.net_io_counters().bytes_sent
-    #bytes_total = bytes_received + bytes_sent
 
     st.write(starName)
     catalogData = Catalogs.query_object(starName, catalog = "TIC")
@@ -41,18 +32,6 @@ def runner(starName):
     coord = SkyCoord(ra, dec, unit = "deg")
     hdulist = Tesscut.get_cutouts(coordinates=coord, size=10)
 
-    #new_received = bytes_received - last_received
-    #new_sent = bytes_sent - last_sent
-
-    #mb_new_received = new_received / 1024 / 1024
-    #mb_new_sent = new_sent / 1024 / 1024
-    #mb_new_total = new_total / 1024 / 1024
-
-    #st.write(mb_new_received)
-    #st.write(mb_new_sent)
-
-    #last_received = bytes_received
-    #last_sent = bytes_sent
 
 list=["GM Aur", "Lambda Ori", "Sz 19", "25 Ori", "Gamma Vel", "HL Tau", "BP Tau", "Sigma Ori", "Epsilon Ori", "TX Ori"]
 
