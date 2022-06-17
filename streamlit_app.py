6d5096b90f76a5060f4f9e1a2d0df91fad305bdf
Javiserna <hipc04.javier@gmail.com>
2022-06-17 12:16:48 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.header("bandwidth usage by astropy")
 last_received = psutil.net_io_counters().bytes_recv
 last_sent = psutil.net_io_counters().bytes_sent
 last_total = last_received + last_sent
+
+
+
