75524bfd3aef1b1d792f11b4833702ac6e9db5d3
Javiserna <hipc04.javier@gmail.com>
2022-06-17 13:07:17 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,16 @@ last_received = psutil.net_io_counters().bytes_recv
 last_sent = psutil.net_io_counters().bytes_sent
 last_total = last_received + last_sent
 
-for i in range(1000):
+while True:
     bytes_received = psutil.net_io_counters().bytes_recv
     bytes_sent = psutil.net_io_counters().bytes_sent
     bytes_total = bytes_received + bytes_sent
 
     new_received = bytes_received - last_received
     new_sent = bytes_sent - last_sent
-    
+
+    mb_new_received = new_received / 1024 / 1024
+    mb_new_sent = new_sent / 1024 / 1024
+    mb_new_total = new_total / 1024 / 1024
+
+    st.write(mb_new_received)
