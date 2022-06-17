73c9c7d5a65203063925ab9c70b014e021ba8620
Javiserna <hipc04.javier@gmail.com>
2022-06-17 16:07:37 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,6 @@ for i in list:
     a.append(tac())
 
 in1 = plt.figure(1)
-plt.hist(a, bins=auto, color = 'red')
+plt.hist(a, bins='auto', color = 'red')
 plt.xlabel('Tiempo (Segundos)')
 st.pyplot(in1)
