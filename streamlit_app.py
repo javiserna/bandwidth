88f20ad906fb6600b7507156d02e9fbf2de88dc1
Javiserna <hipc04.javier@gmail.com>
2022-06-17 13:40:34 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
-import psutil
+#import psutil
 import time
 from astroquery.mast import Tesscut
 from astroquery.mast import Catalogs
 from astropy.coordinates import SkyCoord
+from time import process_time
 
 st.header("bandwidth usage by astropy")
 
-last_received = psutil.net_io_counters().bytes_recv
-last_sent = psutil.net_io_counters().bytes_sent
-last_total = last_received + last_sent
+t1_start = process_time()
+#last_received = psutil.net_io_counters().bytes_recv
+#last_sent = psutil.net_io_counters().bytes_sent
+#last_total = last_received + last_sent
 
 def runner(starName):
-    bytes_received = psutil.net_io_counters().bytes_recv
-    bytes_sent = psutil.net_io_counters().bytes_sent
-    bytes_total = bytes_received + bytes_sent
+    #bytes_received = psutil.net_io_counters().bytes_recv
+    #bytes_sent = psutil.net_io_counters().bytes_sent
+    #bytes_total = bytes_received + bytes_sent
 
 
     catalogData = Catalogs.query_object(starName, catalog = "TIC")
@@ -25,20 +27,26 @@ def runner(starName):
     coord = SkyCoord(ra, dec, unit = "deg")
     hdulist = Tesscut.get_cutouts(coordinates=coord, size=10)
 
-    new_received = bytes_received - last_received
-    new_sent = bytes_sent - last_sent
+    #new_received = bytes_received - last_received
+    #new_sent = bytes_sent - last_sent
 
-    mb_new_received = new_received / 1024 / 1024
-    mb_new_sent = new_sent / 1024 / 1024
+    #mb_new_received = new_received / 1024 / 1024
+    #mb_new_sent = new_sent / 1024 / 1024
     #mb_new_total = new_total / 1024 / 1024
 
-    st.write(mb_new_received)
-    st.write(mb_new_sent)
+    #st.write(mb_new_received)
+    #st.write(mb_new_sent)
 
-    last_received = bytes_received
-    last_sent = bytes_sent
+    #last_received = bytes_received
+    #last_sent = bytes_sent
 
 list=["HL Tau", "BP Tau", "Sigma Ori", "Epsilon Ori", "TX Ori"]
 
 for i in list:
     runner(i)
+
+t1_stop = process_time()
+
+final = t1_stop-t1_start
+
+st.write(final)
