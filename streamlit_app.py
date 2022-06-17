d36e80ec9abcaff94be1eaa5028524858e0a4a00
Javiserna <hipc04.javier@gmail.com>
2022-06-17 12:59:56 -0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,11 @@ last_received = psutil.net_io_counters().bytes_recv
 last_sent = psutil.net_io_counters().bytes_sent
 last_total = last_received + last_sent
 
-ho
+for i in range(1000):
+    bytes_received = psutil.net_io_counters().bytes_recv
+    bytes_sent = psutil.net_io_counters().bytes_sent
+    bytes_total = bytes_received + bytes_sent
+
+    new_received = bytes_received - last_received
+    new_sent = bytes_sent - last_sent
+    
