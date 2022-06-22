80d64b2de3766471c2f6a313a36f9f49040f3e46
Javier Serna <hipc04.javier@gmail.com>
2022-06-21 18:18:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,67 +1,20 @@
-import streamlit as st
-import time
 from astroquery.mast import Tesscut
-from astroquery.mast import Catalogs
-from astropy.coordinates import SkyCoord
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+from astropy.io import fits
+import time
 import random
+import streamlit as st
 
-hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                header {visibility: hidden;}
-                footer {visibility: hidden;}
-                div[data-testid="stToolbar"] {visibility: hidden;}
-                div[data-testid="stDecoration"] {visibility: hidden;}
-                div[data-testid="stStatusWidget"] {visibility: hidden;}
-                </style>
-                """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.header('TESScut download time. Randomize sectors and target sources')
 
-st.header("Tiempo de descarga promedio de datos para 10 estrellas")
-
-_start_time = time.time()
-
-def tic():
-    global _start_time
-    _start_time = time.time()
-
-def tac():
-    t_sec = round(time.time() - _start_time)
-    (t_min, t_sec) = divmod(t_sec,60)
-    (t_hour,t_min) = divmod(t_min,60)
-    st.write('{} seg'.format(t_sec))
-    return t_sec
-
-@st.experimental_memo(suppress_st_warning=True, show_spinner=False)
-def runner(starName):
-    cl1, cl2 = st.columns([1.5,5])
-    with cl1:
-        st.info(starName)
-    catalogData = Catalogs.query_object(starName, catalog = "TIC")
-    ra = catalogData[0]['ra']
-    dec = catalogData[0]['dec']
-    tic = catalogData[0]['ID']
-    Tmag = catalogData[0]['Tmag']
-    coord = SkyCoord(ra, dec, unit = "deg")
-    hdulist = Tesscut.get_cutouts(coordinates=coord, size=10)
-    st.legacy_caching.caching.clear_cache()
-
-
-list=["GM Aur", "Lambda Ori", "Sz 19", "25 Ori", "Gamma Vel", "HL Tau", "BP Tau", "Sigma Ori", "Epsilon Ori", "TX Ori"]
-rand = random.sample(list, len(list))
-
-a=[]
-
-for i in rand:
-    tic()
-    runner(i)
-    a.append(tac())
-
-in1 = plt.figure(1)
-plt.hist(a, color='r')
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Num estrellas')
-st.pyplot(in1)
+for i in range(1000):
+    sources=['HL Tau', 'BP Tau', 'LkCa 4', 'LkCa 9', 'Sigma Ori', 'GM Aur', 'TX Ori', 'FX Ori', 'XX Cha', 'Sz 19']
+    starName = random.choice(sources)
+    sectorTable = Tesscut.get_sectors(objectname=starName)
+    sectors = [i for i in sectorTable['sector']]
+    option = random.choice(sectors)
+    start=time.time()
+    hdulist = Tesscut.get_cutouts(objectname=starName, size=10, sector=option)
+    hdu1 = hdulist[0]
+    end=time.time()
+    elapsed=(end-start)
+    st.write(elapsed)
